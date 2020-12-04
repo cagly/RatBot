d5fbe44e27e061d1d23813e334dc5c25317de898
Hans <haneri@ttu.ee>
2020-12-04 15:07:33 +0200

    a

---
+++ src/main/java/bot/slave/Bot.java
@@ -0,0 +1,14 @@
+package bot.slave;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import java.util.Collection;
+
+public class Bot {
+
+    public static void main(String[] args) {
+        Collection<GatewayIntent> intents
+        JDABuilder builder = JDABuilder.createDefault("Nzg0Mzk3MDUwNjczNDk2MDc0.X8osrg.ErY7oMOINGOTi1IVNk7JidGoJ5Y").
+    }
+}
