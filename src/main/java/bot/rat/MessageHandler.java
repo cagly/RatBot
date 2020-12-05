139189090647ee3d276fc739818e961dfd31b8f7
haneri <haneri@ttu.ee>
2020-12-05 06:19:44 +0200

    Correct link in code command

--- src/main/java/bot/rat/MessageHandler.java
+++ src/main/java/bot/rat/MessageHandler.java
@@ -147,7 +147,7 @@ public class MessageHandler {
             cheese = false;
             event.getMessage().getChannel().sendMessage("Cheese deactivated").queue();
         }  else if (message.equals("code")) {
-            event.getMessage().getChannel().sendMessage("https://github.com/cagly/SlaveBot").queue();
+            event.getMessage().getChannel().sendMessage("https://github.com/cagly/RatBot").queue();
         } else if (message.equals("tell us a joke")) {
             event.getMessage().getChannel().sendMessage(jokes.getJoke()).queue();
         }
