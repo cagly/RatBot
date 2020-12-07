3a66e240b5f8d490c3848b42d16ba1155fb5cd64
haneri <haneri@ttu.ee>
2020-12-07 09:03:59 +0200

    Move resources around

--- src/main/java/bot/rat/JpaConfiguration.java
+++ src/main/java/bot/rat/JpaConfiguration.java
@@ -1,5 +1,6 @@
 package bot.rat;
 
+import bot.rat.privateResources.DatabaseInfo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
---
+++ src/main/java/bot/rat/privateResources/DatabaseInfo.java
@@ -0,0 +1,6 @@
+package bot.rat.privateResources;
+
+public class DatabaseInfo {
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
+}
