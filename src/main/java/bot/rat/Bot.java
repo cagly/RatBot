2c68db68b8345c5d7269ee607d34ca1d438d2108
haneri <haneri@ttu.ee>
2021-08-31 18:04:55 +0300

    Push code to new instance 2

--- src/main/java/bot/rat/Bot.java
+++ src/main/java/bot/rat/Bot.java
@@ -64,7 +64,7 @@ public class Bot extends ListenerAdapter {
                 .build();
         jda.addEventListener(bot);
         jda.awaitReady();
-//        sussy baka code?
+//        sussy baka code
         jda.getTextChannelsByName("bot-test", true).get(0).sendMessage("RatBot is back online!").complete(true);
         bot.messageHandler.startup();
         bot.reminderService.startUp(jda);
