5df2d97f03168da69cc8977014415ebc12661f20
haneri <haneri@ttu.ee>
2021-09-03 15:16:50 +0300

    Add work ID

--- src/main/java/bot/rat/entities/Word.java
+++ src/main/java/bot/rat/entities/Word.java
@@ -1,15 +1,21 @@
 package bot.rat.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Word {
 
     @Id
     private String word;
+    @Id
     @ManyToOne
     @JoinColumn(name = "user_id")
     private UserEntity user;
